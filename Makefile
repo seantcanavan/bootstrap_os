74246ebcfc8ee173bed4f32f582e0779c50bc0ca
seantcanavan@gmail.com <seantcanavan@gmail.com>
2024-12-05 12:05:24 -0600

    fix issues with make, running, sudo, missing packages, and comment out gpg keys for now

--- Makefile
+++ Makefile
@@ -4,10 +4,16 @@ SHELL := /bin/bash
 .PHONE: ubuntu python deps
 
 deps:
-	source venv/bin/activate && pip install -r requirements.txt
+	source .venv/bin/activate && pip install -r requirements.txt
+
+freeze:
+	source .venv/bin/activate && pip freeze -> requirements.txt
+
+pip:
+	source .venv/bin/activate && pip install --upgrade pip
 
 python:
 	python3 -m venv venv
 
 ubuntu:
-	sudo python3 ubuntu/ubuntu.py
+	sudo /home/userhome/code/github.com/seantcanavan/bootstrap_os/.venv/bin/python3 ubuntu.py
--- ubuntu.py
+++ ubuntu.py
@@ -3,15 +3,16 @@ import os
 from git import add_git_auto_setup_remote, add_git_user_and_email
 from install_slack import download_slack_deb
 from install_ungoogled_chromium import download_ungoogled_chromium_deb
-from snap import remove_snap_services, remove_snap_folders
+from snap import remove_snap_folders, remove_snap_services
 from utils import exec_print, exec_strip_split, prt_plus
 
-logged_in_user = exec_strip_split(["whoami"])
+logged_in_user = input("What's your username?")
+print(f"logged_in_user: {logged_in_user}")
 
 add_git_user_and_email(username=logged_in_user)
 add_git_auto_setup_remote(username=logged_in_user)
-# remove_snap_services()
-# remove_snap_folders(username=logged_in_user)
+remove_snap_services()
+remove_snap_folders(username=logged_in_user)
 
 apt_setup_commands = [
     ["wget", "-N", "https://download.sublimetext.com/sublimehq-pub.gpg"],
@@ -85,69 +86,69 @@ prt_plus("Installing new packages")
 for new_package in apt_new_packages:
     exec_print(["sudo", "apt-get", "install", "-y", new_package])
 
-prt_plus("Setting up Git")
-ssh_priv_file = "/home/" + logged_in_user + "/.ssh/id_rsa"
-ssh_pub_file = "/home/" + logged_in_user + "/.ssh/id_rsa.pub"
-
-
-if os.path.exists(ssh_priv_file):
-    exec_print(
-        [
-            "sudo",
-            "-u",
-            logged_in_user,
-            "ssh-keygen",
-            "-t",
-            "rsa",
-            "-b",
-            "4096",
-            "-f",
-            ssh_priv_file,
-            '-N ""',
-        ]
-    )
-
-exec_print(["cat", ssh_pub_file])
-input("Your ssh public key was just printed. Copy it to github and press enter when done.").strip()
-
-github_username = input("Enter your github username: ")
-github_pat = input("Enter your github PAT. This will be used to unlock calls to private repos. ")
-exec_print(
-    [
-        "sudo",
-        "-u",
-        logged_in_user,
-        "git",
-        "config",
-        "--global",
-        "url.https://{0}:{1}@github.com.insteadOf".format(github_username, github_pat),
-        "https://github.com",
-    ]
-)
-
-
-if user_email not in exec_strip_split(["sudo", "-u", logged_in_user, "gpg", "--list-keys", "--with-colons"]):
-    print(f"No GPG key found for email {user_email}. Creating one now.")
-    gpg_file_lines = [
-        "Key-Type: RSA",
-        "Key-Length: 4096",
-        f"Name-Real: {user_name}",
-        f"Name-Email: {user_email}",
-        "Expire-Date: 1y",
-    ]
-
-    passphrase = input("Enter your passphrase for the gpg key")
-
-    gpg_file_lines.append("Passphrase: " + passphrase)
-    gpg_file_lines.append("%commit")
-    gpg_file_lines.append("%echo done")
-
-    gpg_file_name = "gpg_config_file"
-
-    with open("gpg_config_file", "w") as gpg_config_file:
-        gpg_config_file.writelines([line + "\n" for line in gpg_file_lines])
-
-    exec_print(["sudo", "-u", logged_in_user, "gpg", "--gen-key", "--batch", gpg_file_name])
-
-exec_print(["sudo", "-u", logged_in_user, "gpg", "--armor", "--export", user_email])
-print("Your GPG key has been printed. Paste it into github and hit enter to continue.")
+# prt_plus("Setting up Git")
+# ssh_priv_file = "/home/" + logged_in_user + "/.ssh/id_rsa"
+# ssh_pub_file = "/home/" + logged_in_user + "/.ssh/id_rsa.pub"
+#
+#
+# if os.path.exists(ssh_priv_file):
+#     exec_print(
+#         [
+#             "sudo",
+#             "-u",
+#             logged_in_user,
+#             "ssh-keygen",
+#             "-t",
+#             "rsa",
+#             "-b",
+#             "4096",
+#             "-f",
+#             ssh_priv_file,
+#             '-N ""',
+#         ]
+#     )
+#
+# exec_print(["cat", ssh_pub_file])
+# input("Your ssh public key was just printed. Copy it to github and press enter when done.").strip()
+#
+# github_username = input("Enter your github username: ")
+# github_pat = input("Enter your github PAT. This will be used to unlock calls to private repos. ")
+# exec_print(
+#     [
+#         "sudo",
+#         "-u",
+#         logged_in_user,
+#         "git",
+#         "config",
+#         "--global",
+#         "url.https://{0}:{1}@github.com.insteadOf".format(github_username, github_pat),
+#         "https://github.com",
+#     ]
+# )
+#
+#
+# if user_email not in exec_strip_split(["sudo", "-u", logged_in_user, "gpg", "--list-keys", "--with-colons"]):
+#     print(f"No GPG key found for email {user_email}. Creating one now.")
+#     gpg_file_lines = [
+#         "Key-Type: RSA",
+#         "Key-Length: 4096",
+#         f"Name-Real: {user_name}",
+#         f"Name-Email: {user_email}",
+#         "Expire-Date: 1y",
+#     ]
+#
+#     passphrase = input("Enter your passphrase for the gpg key")
+#
+#     gpg_file_lines.append("Passphrase: " + passphrase)
+#     gpg_file_lines.append("%commit")
+#     gpg_file_lines.append("%echo done")
+#
+#     gpg_file_name = "gpg_config_file"
+#
+#     with open("gpg_config_file", "w") as gpg_config_file:
+#         gpg_config_file.writelines([line + "\n" for line in gpg_file_lines])
+#
+#     exec_print(["sudo", "-u", logged_in_user, "gpg", "--gen-key", "--batch", gpg_file_name])
+#
+# exec_print(["sudo", "-u", logged_in_user, "gpg", "--armor", "--export", user_email])
+# print("Your GPG key has been printed. Paste it into github and hit enter to continue.")
